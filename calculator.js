d905650e1b3c90291b2519c09551085b3c7e8bef
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-12-28 19:04:17 -0500

    Create initial html, css and js files
    Created a blank html page with a main div and
    created calculator.js with initial operate, add,
    subtract, multiply & divide functions

---
+++ calculator.js
@@ -0,0 +1,40 @@
+let main = document.querySelector(".main");
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator, num1, num2) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
