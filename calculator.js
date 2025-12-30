19b5f982ce72449a18ad75714ba1eef5fbf1726d
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-12-29 23:08:16 -0500

    Initial simple calculator creation (no chaining)

--- calculator.js
+++ calculator.js
@@ -1,21 +1,48 @@
 const main = document.querySelector(".main");
 
-let num1;
-let num2;
-let operator;
-
 const buttons = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', 'x', '÷', '=', 'C'];
-const buttonObjects = [];
 
 const display = document.createElement("div");
-display.textContent = "3 + 5";
 main.appendChild(display);
 
 buttons.forEach((value) => {
     const buttonObject = createButton(value);
+    buttonObject.button.addEventListener("click", function (e) {
+        switch (value) {
+            case "=":
+                parseValues();
+                break;
+            case "C":
+                clear();
+                break;
+            default:
+                display.textContent += value;
+        }
+    });
     main.appendChild(buttonObject.button);
-    buttonObjects.push(buttonObject);
 });
+
+function parseValues() {
+    let content = display.textContent.split(/([+÷\-x])/);
+    let val1 = Number(content[0]);
+    let val2 = Number(content[2]);
+    let operator;
+    switch (content[1]) {
+        case "+":
+            operator = "+";
+            break;
+        case "-":
+            operator = "-";
+            break;
+        case "x":
+            operator = "*";
+            break;
+        case "÷":
+            operator = "/";
+            break;
+    }
+    display.textContent = operate(operator, val1, val2);
+}
 
 function createButton(value) {
     const button = document.createElement("button");
@@ -24,6 +51,11 @@ function createButton(value) {
         button,
         value
     };
+}
+
+function clear() {
+    display.textContent = "";
+    return;
 }
 
 function operate(operator, num1, num2) {
